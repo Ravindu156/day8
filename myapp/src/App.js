72bea25753a69e07b26dd28763e4aa5d1b381faa
Ravindu Hirusanka <puresoftlk@gmail.com>
2024-11-26 08:35:03 +0000

    Add routing for Home, About, and Contact pages in App component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import {BrowserRouter as Router,Routes,Route,Link,NavLink} from 'react-router-dom';
+import ContactUs from './components/ContactUs';
 
 function App() {
   return (
     <div className="App">
-  
+      <Router>
+        <Routes>
+            <Route path='/home' element={<Home/>}></Route>
+            <Route path='/about' element={<AboutUs/>}></Route>
+            <Route path='/contact' element={<ContactUs/>}></Route>
+          </Routes>  
+        
+       </Router>  
     </div>
   );
 }
