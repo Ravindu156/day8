95323292acbf18fd3942f2e726c3f245b6ba77cf
Ravindu Hirusanka <puresoftlk@gmail.com>
2024-11-26 08:38:20 +0000

    Add Home and AboutUs components to App for routing

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import {BrowserRouter as Router,Routes,Route,Link,NavLink} from 'react-router-dom';
 import ContactUs from './components/ContactUs';
+import Home from './components/Home';
+import AboutUs from './components/AboutUs';
 
 function App() {
   return (
