b5eba1156bd8726acdfdaf49647bb6b286a4aef3
Ravindu Hirusanka <puresoftlk@gmail.com>
2024-11-26 09:37:09 +0000

    Implement user authentication in Login component with state management and error handling

--- myapp/src/components/Login.js
+++ myapp/src/components/Login.js
@@ -1,20 +1,38 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 export default function Login(){
     const navigate=useNavigate();
+   
+    
+    
     const authenticate=()=>{
-        navigate('/dash');
+        if(user===username && pass===password){
+            navigate('/dash');
+        }
+        else{
+            alert('Invalid Credentials');
+        }
 
     }
 
+    const [user,setUser]=useState({
+        username:''
+    });  
+    const [pass,setPass]=useState({
+        password:''
+    });
 
-
-
+    const username='admin';
+    const password='admin';
 
     return(
         <div>
             <h1>Login Here</h1>
             <p>Username:</p>
-            <input type="text"></input>
+            <input type="text" value={setUser} name="usrname" id="usrname"></input>
+            <p>Password</p>
+            <input type="password" value={setPass} name='pass1' id="pass1"></input>
+            <br></br>
             <button onClick={authenticate}>Login</button>
         </div>
 
